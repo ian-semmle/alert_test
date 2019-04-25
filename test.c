29298e4fb9b0a439fb9f6f3fa2864761a0c50833
Ian Lynagh <ian@semmle.com>
2019-04-25 19:00:10 +0100

    Change

--- test.c
+++ test.c
@@ -1,14 +1,8 @@
 
-int vbaz;
+int vbaq;
 
-void fun_baz(int vbaz) {
+void fun_baq(int vbaq) {
 }
-
-#define FUNX fun_xxx1
-#define NAMEX vxxx1
-#include "a.h"
-#undef NAMEX
-#undef FUNX
 
 #define FUNX fun_xxx2
 #define NAMEX vxxx2
@@ -16,14 +10,26 @@ void fun_baz(int vbaz) {
 #undef NAMEX
 #undef FUNX
 
+#define FUNX fun_xxx1
+#define NAMEX vxxx1
+#include "a.h"
+#undef NAMEX
+#undef FUNX
+
+#define FUNY fun_yyy2
+#define NAMEY vyyy2
+#include "b.h"
+#undef NAMEY
+#undef FUNY
+
 #define FUNY fun_yyy1
 #define NAMEY vyyy1
 #include "b.h"
 #undef NAMEY
 #undef FUNY
 
-#define FUNY fun_yyy2
-#define NAMEY vyyy2
+#define FUNY fun_yyy3
+#define NAMEY vyyy3
 #include "b.h"
 #undef NAMEY
 #undef FUNY
